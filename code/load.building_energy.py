e26cd9053afae1fd7fd388cfaf9c441caa1b97ad
pelayolartategui <pelayolartategui@gmail.com>
2017-10-08 11:53:54 +0200

    building_energy names

--- code/load.building_energy.py
+++ code/load.building_energy.py
@@ -1,16 +1,27 @@
 import os
+
 import h2o
+import matplotlib.pyplot as plt
+
 h2o.init()
 
 #If next line fails, instead set path to location of datasets.
 path = os.path.dirname(__file__)
 fname = os.path.join(path, "../datasets/ENB2012_data.csv")
 data = h2o.import_file(fname)
-
-factorsList = ["X6","X8"]
+x = data.names
+predictions = ["Heating_Load", "Cooling_Load"]
+factorsList = ["Orientation", "Glazing_Area_Dist"]
 data[factorsList] = data[factorsList].asfactor()
-
+x.remove(predictions[0])
+x.remove(predictions[1])
 train, test = data.split_frame([0.8])
 
-x = ["X1", "X2", "X3", "X4", "X5", "X6", "X7", "X8"]
-y = "Y2"  #Or "Y1"
+train.describe()
+
+print(train.cor().round(2))
+
+res = train[x].cor(train[predictions[0]]).as_data_frame()
+res.index = x
+res.plot.barh()
+plt.show()
